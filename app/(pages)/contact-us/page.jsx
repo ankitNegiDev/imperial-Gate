23d20afab9cae3c4c5e017427d7ed29def8f06d8
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-01 15:53:20 +0530

    updated the contact-us page

--- app/(pages)/contact-us/page.jsx
+++ app/(pages)/contact-us/page.jsx
@@ -1,11 +1,14 @@
 import FloatingButtons from "../../components/home-page/FloatingButtons";
 import ContactUs from "../../components/contact-us/ContactUs";
+import FAQ from "../../components/home-page/Faq";
+
 
 export default function Contact() {
     return (
         <main className="pt-20">
             <ContactUs />
             <FloatingButtons/>
+            <FAQ/>
         </main>
     );
 }
