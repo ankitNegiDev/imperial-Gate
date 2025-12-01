4d3dd2914ec382abc911f9ec1054eea33fd7a974
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-01 15:52:53 +0530

    udpated the blog-page

--- app/(pages)/blogs/page.jsx
+++ app/(pages)/blogs/page.jsx
@@ -1,6 +1,8 @@
 
 import FloatingButtons from "../../components/home-page/FloatingButtons";
 import Blogs from "../../components/blogs/Blogs";
+import FAQ from "../../components/home-page/Faq";
+
 function Blog(){
 
     return(
@@ -8,6 +10,7 @@ function Blog(){
             {/* <h1 className="pt-50 text-center text-4xl">Blogs page coming sooon</h1> */}
             <Blogs/>
             <FloatingButtons/>
+            <FAQ/>
         </>
     );
 }
