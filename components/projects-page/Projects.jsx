fd6ac1ec8945661d3e0b69aed724b973082dd118
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-03 20:23:16 +0530

    added the coming soon page on project page

---
+++ components/projects-page/Projects.jsx
@@ -0,0 +1,17 @@
+import FAQ from "../home-page/Faq";
+
+function Projects(){
+
+
+
+    return(
+        <>
+            <div className="bg-black  h-[30vh]">
+                <h1 className="text-7xl text-white font-bold  text-center mt-33 mb-33 bg-black p-10">coming soon Projects page</h1>
+
+            </div>
+            <FAQ/>
+         </>
+    );
+}
+export default Projects;
