ba67121a8140fca3efcce8a642a263ed949a291e
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-03 20:26:59 +0530

    updated the projects page

---
+++ app/(pages)/projects/page.jsx
@@ -0,0 +1,15 @@
+// import Projects from "@/components/projects/Projects";
+
+// import Projects from "../../components/projects/Projects"
+
+import Projects from "../../../components/projects-page/Projects"
+function ProjectsPage(){
+
+    return(
+        <>
+            <Projects />
+        </>
+    );
+
+}
+export default ProjectsPage
--- app/(pages)/store/page.jsx
+++ app/(pages)/store/page.jsx
@@ -1,14 +1,18 @@
 
-import FloatingButtons from "../../components/home-page/FloatingButtons"
+// import FloatingButtons from "../../components/home-page/FloatingButtons"
 // import Store from "../../components/ecom-store/Store1"
-import StorePage from "../../components/ecom-store/StorePage"
+// import StorePage from "../../components/ecom-store/StorePage"
+// import StorePage from "@/components/ecom-store/StorePage"
+// import FloatingButtons from "@/components/home-page/FloatingButtons"
 
+import StorePage from "../../../components/ecom-store/StorePage"
+import FloatingButtons from "../../../components/home-page/FloatingButtons"
 function EcomStore(){
 
 
     return(
         <>
-            <StorePage/>
+            <StorePage />
             <FloatingButtons />
         </>
     );
