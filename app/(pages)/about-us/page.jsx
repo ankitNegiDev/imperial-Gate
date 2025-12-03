44e04fdb3cfd5161a57093cd5dd7d5e10e4b7d17
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-03 20:24:48 +0530

    updated the about us page

--- app/(pages)/about-us/page.jsx
+++ app/(pages)/about-us/page.jsx
@@ -1,8 +1,11 @@
-import FloatingButtons from "../../components/home-page/FloatingButtons";
-import AboutUs from "../../components/about-us/AboutUs";
-import FAQ from "../../components/home-page/Faq";
+// import FloatingButtons from "../../components/home-page/FloatingButtons";
+// import AboutUs from "../../components/about-us/AboutUs";
+// import FAQ from "../../components/home-page/Faq";
 
 
+import AboutUs from "../../../components/about-us/AboutUs"
+import FloatingButtons from "../../../components/home-page/FloatingButtons"
+import FAQ from "../../../components/home-page/Faq"
 export default function Contact() {
     return (
         <main className="pt-20">
