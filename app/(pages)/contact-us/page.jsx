ecf837e156d7eb48f7aff9385e90aaf5b8af57c6
ankitNegiDev <ankitnegi9104@gmail.com>
2025-12-03 20:25:43 +0530

    updated the contact us page

--- app/(pages)/contact-us/page.jsx
+++ app/(pages)/contact-us/page.jsx
@@ -1,6 +1,14 @@
-import FloatingButtons from "../../components/home-page/FloatingButtons";
-import ContactUs from "../../components/contact-us/ContactUs";
-import FAQ from "../../components/home-page/Faq";
+// import FloatingButtons from "../../components/home-page/FloatingButtons";
+// import ContactUs from "../../components/contact-us/ContactUs";
+// import FAQ from "../../components/home-page/Faq";
+
+// import ContactUs from "@/components/contact-us/ContactUs";
+// import FAQ from "@/components/home-page/Faq";
+// import FloatingButtons from "@/components/home-page/FloatingButtons";
+
+import ContactUs from "../../../components/contact-us/ContactUs"
+import FAQ from "../../../components/home-page/Faq"
+import FloatingButtons from "../../../components/home-page/FloatingButtons"
 
 
 export default function Contact() {
